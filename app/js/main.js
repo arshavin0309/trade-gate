545b36f1b80d1da8b4d4748dc9285fb1a06a52de
Владислав <arshavin0309@mail.ru>
2024-10-25 15:18:45 +0500

    add gulp

---
+++ app/js/main.js
@@ -0,0 +1,14 @@
+// Установка title, для wp не понадобиться
+if (document.location.pathname === '/') { document.title = 'Главная' };
+if (document.location.pathname === '/404.html') { document.title = 'Страница не найдена' };
+if (document.location.pathname === '/terms-of-trade.html') { document.title = 'Условия торговли' };
+if (document.location.pathname === '/platform.html') { document.title = 'Платформа' };
+if (document.location.pathname === '/analytics.html') { document.title = 'Аналитика рынка' };
+if (document.location.pathname === '/news.html') { document.title = 'Новости рынка' };
+if (document.location.pathname === '/forecasts.html') { document.title = 'Прогнозы рынка' };
+if (document.location.pathname === '/currencies.html') { document.title = 'Валюты' };
+if (document.location.pathname === '/cryptocurrencies.html') { document.title = 'Криптовалюты' };
+if (document.location.pathname === '/stocks.html') { document.title = 'Акции' };
+if (document.location.pathname === '/indexes.html') { document.title = 'Индексы' };
+if (document.location.pathname === '/about.html') { document.title = 'О Patronus Investments' };
+if (document.location.pathname === '/contacts.html') { document.title = 'Контакты' };
