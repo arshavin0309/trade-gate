57686117b9a72e5aa811fd948135d43be241f9bd
Владислав <arshavin0309@mail.ru>
2024-12-06 10:26:27 +0500

    add

--- app/js/main.js
+++ app/js/main.js
@@ -108,8 +108,16 @@ let swiper7 = new Swiper(".swiper7", {
 });
 
 let swiper8 = new Swiper(".swiper8", {
-    slidesPerView: 3,
     spaceBetween: 20,
+
+    breakpoints: {
+        600: {
+            slidesPerView: 2,
+        },
+        992: {
+            slidesPerView: 3,
+        },
+    },
 
     pagination: {
         el: ".swiper-pagination",
